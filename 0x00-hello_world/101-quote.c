1b089b477e2de6681180e26c708fc0e34f7a69f2
MohammedShanan <87578117+MohammedShanan@users.noreply.github.com>
2022-10-13 17:02:39 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  *main - Entry point
  *Return: Always 1
-*/
+ */
 
 int main(void)
 {
-	char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
-	fwrite(hello, sizeof(hello) - 1, 1, stdout);
+	char qoute[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(qoute, sizeof(qoute) - 1, 1, stdout);
 	return (1);
 }
