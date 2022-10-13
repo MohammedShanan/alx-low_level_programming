789f84f72436e569f02bf92e9cfa6dbcc04c58cf
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-13 16:47:42 +0200

    solved 8 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 1
+*/
+
+int main()
+{
+	char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(hello, sizeof(hello) - 1, 1, stdout);
+	return (0);
+}
