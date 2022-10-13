f7fc7a557bd0706daf521d08f46d722083d280a8
MohammedShanan <87578117+MohammedShanan@users.noreply.github.com>
2022-10-13 14:33:51 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ *use puts to used to write a line or string to the output stream
+ */
 
 int main(void)
 {
-     puts("\"Programming is like building a multilingual puzzle");
-     return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
