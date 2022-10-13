741acbc827a2e2d16375404cc8e48cc283aa2a3e
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-13 14:53:51 +0200

    solved 5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - uses printf to write a line or string to the output stream
+ *Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
