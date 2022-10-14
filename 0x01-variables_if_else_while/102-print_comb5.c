98967fc56ce9331dff19d4193219a837dfcc437b
MohammedShanan <87578117+MohammedShanan@users.noreply.github.com>
2022-10-14 18:02:53 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,39 +6,50 @@
 /**
  *main - Entry point
  *Return: 0
-*/
+ */
 int main(void)
 {
-int tens;
-int ones;
-int t;
-int o;
-for (tens = '0'; tens <= '9'; tens++)
-{
-for (ones = '0'; ones <= '9'; ones++)
-{
-for (t = '0'; t <= '9'; t++)
-{
-for (o = ones; o <= '9'; o++)
-{
-if ((t == tens) && (o == ones))
-{
-  continue;
-}
-putchar(tens);
-putchar(ones);
-putchar(' ');
-putchar(t);
-putchar(o);
-if ((tens != '9' || ones != '8') || (t != '9' && o != '9'))
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
-putchar('\n');
-return (0);
+    int tens;
+    int ones;
+    int t;
+    int o;
+    int n = 0;
+    int m = 0;
+    for (tens = '0'; tens <= '9'; tens++)
+    {
+        for (ones = '0'; ones <= '9'; ones++)
+        {
+        // if  (ones == '1')
+        // {
+        //     putchar('\n');
+        // }
+            for (t = '0'; t <= '9'; t++)
+            {
+
+                for (o = '0' + n; o <= '9'; o++)
+                {
+                    if ((t == tens) && (o == ones))
+                    {
+                        continue;
+                    }
+                    putchar(tens);
+                    putchar(ones);
+                    putchar(' ');
+                    putchar(t);
+                    putchar(o);
+                    if ((tens != '9' || ones != '8') || (t != '9' && o != '9'))
+                    {
+                        putchar(',');
+                        putchar(' ');
+                    }
+                }
+            n = 0;
+            }
+            m++;
+            n = m;
+        }
+        
+    }
+    putchar('\n');
+    return (0);
 }
