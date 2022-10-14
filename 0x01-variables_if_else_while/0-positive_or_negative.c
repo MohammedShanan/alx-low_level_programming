f77dd31673a44dffce917a3d166e12c7e13a589d
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-14 11:09:35 +0200

    updated 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
 {
-printf(" %i is negative", n);
+printf("%i is negative ", n);
 }
 else if (n > 0)
 {
-printf(" %i is positive", n);
+printf("%i is positive ", n);
 }
 else
 {
-printf(" 0 is zero");
+printf("0 is zero ");
 }
 return (0);
 }
