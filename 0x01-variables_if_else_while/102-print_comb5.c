314ee9afd882022c0d2ee4d553fbdb6d2ad5ec98
MohammedShanan <87578117+MohammedShanan@users.noreply.github.com>
2022-10-14 21:47:32 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,36 +6,35 @@
 /**
  *main - Entry point
  *Return: 0
- */
+*/
 int main(void)
 {
-    int tens;
-    int ones;
-    int t;
-    int o;
-    for (tens = '0'; tens <= '9'; tens++)
-    {
-        for (ones = '0'; ones <= '9'; ones++)
-        {
-            for (t = '0'; t <= '9'; t++)
-            {
-
-                for (o = ones + 1; o <= '9'; o++)
-                {
-                    putchar(tens);
-                    putchar(ones);
-                    putchar(' ');
-                    putchar(t);
-                    putchar(o);
-                    if ((tens != '9' || ones != '8') || (t != '9' && o != '9'))
-                    {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                }
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+int tens;
+int ones;
+int t;
+int o;
+for (tens = '0'; tens <= '9'; tens++)
+{
+for (ones = '0'; ones <= '9'; ones++)
+{
+for (t = tens; t <= '9'; t++)
+{
+for (o = ones; o <= '9'; o++)
+{
+putchar(tens);
+putchar(ones);
+putchar(' ');
+putchar(t);
+putchar(o);
+if ((tens != '9' || ones != '8') || (t != '9' && o != '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
