049888fdd049207577abbb0c9ac2e2265df874dc
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-14 09:57:14 +0200

    solved 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ *main - Entry point
+ *Return: 0
+*/
+int main(void)
+{
+char a = 'a';
+for (int i = a; i <= 'z'; i++)
+{
+if (i != 'q' || i != 'e')
+{
+putchar(i);
+}
+}
+putchar('\n');
+return (0);
+}
