efcac31e100ca89008cca611e101d582afd61096
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-14 11:15:09 +0200

    added \n to 0 and 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
 {
-printf("%i is negative ", n);
+printf("%i is negative\n", n);
 }
 else if (n > 0)
 {
-printf("%i is positive ", n);
+printf("%i is positive\n", n);
 }
 else
 {
-printf("0 is zero ");
+printf("0 is zero\n");
 }
 return (0);
 }
