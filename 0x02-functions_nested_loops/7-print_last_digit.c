7150093eb978898757783462261b8ed2882139f0
MohammedShanan <87578117+MohammedShanan@users.noreply.github.com>
2022-10-18 14:11:56 +0200

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,7 @@ int print_last_digit(int n)
 {
 int last_dig;
 last_dig = n % 10;
-last_dig = last_dig > 0 ? last_dig : last_dig * - 1;
+last_dig = last_dig > 0 ? last_dig : last_dig * -1;
 _putchar('0' + last_dig);
 return (last_dig);
 }
