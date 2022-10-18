0065bc33b6462eb9e0972552f9b372fec0199e9a
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-18 16:31:01 +0200

    solved john abruzzi

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of even Fionacci sequence up to 4,000,000
+ * Return: 0
+ */
+
+int main(void)
+{
+long a = 0, b = 1;
+long sum;
+long sum_of_evens = 0;
+for (; sum < 4000000;)
+{
+sum = a + b;
+a = b;
+b = sum;
+if (sum % 2 == 0 && sum < 4000000)
+{
+sum_of_evens += sum;
+}
+}
+printf("%li\n", sum_of_evens);
+return (0);
+}
