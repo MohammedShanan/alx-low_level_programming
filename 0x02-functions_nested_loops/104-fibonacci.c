ffe48a4af72221cf51075f7f647cb67eb2de605b
MohammedShanan <87578117+MohammedShanan@users.noreply.github.com>
2022-10-18 21:47:15 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,23 +3,23 @@
 #include <stdint.h>
 
 /**
- * main - print first 50 Fibonacci numbers
- * Return: 0
- */
+* main - print first 50 Fibonacci numbers
+* Return: 0
+*/
 
 int main(void)
 {
-    uint64_t a = 0, b = 1;
-    int i;
-    uint64_t sum;
-    for (i = 1; i < 98; i++)
-    {
-        sum = a + b;
-        printf("counter is %i,number is %llu\n", i,sum);
-        a = b;
-        b = sum;
-    }
-    sum = a + b;
-    printf("%llu\n", sum);
-    return (0);
+uint64_t a = 0, b = 1;
+int i;
+uint64_t sum;
+for (i = 1; i < 98; i++)
+{
+sum = a + b;
+printf("counter is %i,number is %lu\n", i,sum);
+a = b;
+b = sum;
+}
+sum = a + b;
+printf("%lu\n", sum);
+return (0);
 }
