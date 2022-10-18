b547f01fcc58d2f688d2ba5c70b85fdf4b2a8b4c
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-18 21:44:43 +0200

    solved 104 fi

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include"main.h"
+#include <stdint.h>
+
+/**
+ * main - print first 50 Fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+    uint64_t a = 0, b = 1;
+    int i;
+    uint64_t sum;
+    for (i = 1; i < 98; i++)
+    {
+        sum = a + b;
+        printf("counter is %i,number is %llu\n", i,sum);
+        a = b;
+        b = sum;
+    }
+    sum = a + b;
+    printf("%llu\n", sum);
+    return (0);
+}
