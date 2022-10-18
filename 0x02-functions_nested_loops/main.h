947d4fcb46cfb449d105c3940bf28eca2e34bd28
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-18 10:16:40 +0200

    solved sign.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -30,4 +30,12 @@ void print_alphabet_x10(void);
 
 int _islower(int c);
 
+/**
+ * _isalpha - checks for alphabet character
+ * @c: character to check
+ * Return: 1 if lower or uppercase, 0 if not alphabet character
+ */
+
 int _isalpha(int c);
+
+int print_sign(int n);
