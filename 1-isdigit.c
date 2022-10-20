6c124ddbc21cf5d7f341cb5265022c901265e811
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-20 09:19:32 +0200

    solved isdigit

---
+++ 1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for digit character
+ * @c: character to check
+ * Return: 1 if it's a digit , 0 if otherwise
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '1')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
