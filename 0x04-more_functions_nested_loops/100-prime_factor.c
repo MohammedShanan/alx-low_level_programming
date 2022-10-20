c28f9a91bb2d81d8c9fdf351ef7b95cf984bc7e3
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-20 14:00:28 +0200

    updated style for prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 #include <stdint.h>
+
+/**
+ * main - print largest prime factor of 612852475143
+ * Return: 0 on success
+ */
+
 int main(void)
 {
-    unsigned long i;
-    int largest_prime = 2;
-    unsigned long n = 612852475143;
-    while (n % 2 == 0)
-    {
-        n = n / 2;
-    }
+int largest_prime = 2;
+unsigned long i;
+unsigned long n = 612852475143;
+while (n % 2 == 0)
+{
+n = n / 2;
+}
 
-    for (i = 3; i <= n; i = i + 2)
-    {
-        if (n % i == 0)
-        {
-            largest_prime = i;
-            while (n % i == 0)
-            {
-                n = n / i;
-            }
-        }
-    }
-    largest_prime = largest_prime > 2 ? largest_prime : 2;
-    printf("%d\n", largest_prime);
-    return (0);
+for (i = 3; i <= n; i = i + 2)
+{
+if (n % i == 0)
+{
+largest_prime = i;
+while (n % i == 0)
+{
+n = n / i;
+}
+}
+}
+largest_prime = largest_prime > 2 ? largest_prime : 2;
+printf("%d\n", largest_prime);
+return (0);
 }
