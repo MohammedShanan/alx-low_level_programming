be12a020f33b9e287bb400aaed07a3b174999296
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-24 15:54:23 +0200

    solved print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array
+ * @n: number of elements
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%i", a[i]);
+}
+printf("\n");
+}
