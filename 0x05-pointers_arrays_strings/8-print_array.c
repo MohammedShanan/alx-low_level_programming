ea283a5cf9b0e888a0d97e2ce1ca50dd310f6c1d
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-24 15:58:13 +0200

    solved print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,9 +9,9 @@
 void print_array(int *a, int n)
 {
 int i;
-for (i = 0; i < n; i++)
+for (i = 0; i < n - 1 i++)
 {
-printf("%i", a[i]);
+printf("%i, ", a[i]);
 }
-printf("\n");
+printf("%i\n",a[i]);
 }
