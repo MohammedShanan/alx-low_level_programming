5a604646c060ab1673016e33945aa29fcd77c237
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-24 15:47:58 +0200

    solved half puts

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -20,15 +20,16 @@ return (len);
 
 /**
  * puts_half - prints half of a string, followed by a new line
- * @s: string to reverse
+ * @str: string to reverse
  */
 
 void puts_half(char *str)
 {
 int len;
-char *half_str; 
+char *half_str;
 len = _strlen(str);
 half_str = len / 2 + str;
+half_str = len % 2 == 0 ? half_str : half_str + 1;
 while (*half_str)
 {
 _putchar(*half_str);
