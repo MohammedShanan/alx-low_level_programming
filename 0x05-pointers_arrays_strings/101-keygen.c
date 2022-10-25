e50d55440c852705f177f87e086765be9b9a1135
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-25 08:35:13 +0200

    first try keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    int n;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    printf("random number%i", n);
+}
