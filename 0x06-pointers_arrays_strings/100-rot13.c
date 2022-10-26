333d4b456f5ba399b8bd93178e2181c341037247
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-26 14:43:17 +0200

    solved rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,26 +1,32 @@
-#include<stdio.h>
+
+/**
+ * rot13 - encode string using rot13
+ * @s: string to encode
+ * Return: encoded string
+ */
+
 char *rot13(char *s)
 {
-    int i = 0, n;
-    while (s[i])
-    {
-        while (s[i] >= 'a' && s[i] <= 'z')
-        {
-            n = (s[i] + 13) % ('a' + 26);
-            printf("n is %i char is %c\n", n, s[i]);
+int i = 0, n;
+while (s[i])
+{
+while (s[i] >= 'a' && s[i] <= 'z')
+{
+n = s[i] % 'a';
+n = n + 13;
+n = n % 26;
+s[i] = 'a' + n;
+i++;
+}
+if (s[i] >= 'A' && s[i] <= 'Z')
+{
+n = s[i] % 'A';
+n = n + 13;
+n = n % 26;
+s[i] = 'A' + n;
+}
 
-            s[i] = 'a' + n;
-            i++;
-        }
-        if (s[i] >= 'A' && s[i] <= 'Z')
-        {
-            n = (s[i] + 13) % ('A' + 26);
-            printf("n is %i char is %c\n", n, s[i]);
-            s[i] = 'A' + n ;
-            
-        }
-        
-        i++;
-    }
-    return (s);
+i++;
+}
+return (s);
 }
