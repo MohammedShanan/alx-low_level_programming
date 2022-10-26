9a9f5d6bd60688a962b57ee1f0565bd5ace72397
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-26 10:18:28 +0200

    solved toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,38 @@
+/**
+ * _islower - checks for lowercase character
+ * @c: character to check
+ * Return: 1 if lowercase, 0 if uppercase
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
+/**
+ * string_toupper - capitalize all letters in string
+ * @s: string to manipulate
+ * Return: string with all letters capitalized
+ */
+
+char *string_toupper(char *str)
+{
+int i;
+i = 0;
+while (str[i])
+{
+if (_islower(str[i]))
+{
+str[i] -= 32;
+}
+i++;
+}
+return (str);
+}
