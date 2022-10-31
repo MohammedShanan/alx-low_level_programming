4ba1116472a51d576f29cda2d52eb01cb77c3e1c
MohammedShanan <87578117+MohammedShanan@users.noreply.github.com>
2022-10-31 15:21:29 +0200

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,20 +7,21 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i = 0, j;
-	int matches = 0;
+	int counter = 0;
+	
 	while (*(s + i))
 	{
 		for (j = 0; *(accept + j); j++)
 		{
 			if (*(s + i) == *(accept + j))
 			{
-				matches++;
+				counter++;
 				break;
 			}
 			if (*(accept + j + 1) == '\0' && *(s + i) != *(accept + j))
-				return (matches);
+				return (counter);
 		}
 		i++;
 	}
-	return (matches);
+	return (counter);
 }
