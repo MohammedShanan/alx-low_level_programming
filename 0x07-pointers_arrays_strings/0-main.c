73acf6bf8579aa66b673847cc685905ee3469300
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-31 16:35:16 +0200

    solved chessboard

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -1,5 +1,26 @@
-#include "main.h"
+
 #include <stdio.h>
+#include "main.h"
+/**
+ * _strpbrk - return pointer to byte in s that matches a byte in accept
+ * @s: string to search
+ * @accept: target matches
+ * Return: pointer to index of string at first occurrence
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j = 0;
+
+	while (i < 8)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(*(*(a + i) + j));
+		}
+		putchar('\n');
+		i++;
+	}
+}
 
 /**
  * main - check the code
@@ -8,11 +29,16 @@
  */
 int main(void)
 {
-	char *s = "hello, world";
-	char *f = "world";
-	char *t;
-
-	t = _strstr(s, f);
-	printf("%s\n", t);
+	char board[8][8] = {
+		{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
+		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+		{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+	};
+	print_chessboard(board);
 	return (0);
 }
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print chessboard given set 2D array
+ * @a: 2D array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+        int i = 0, j = 0;
+
+        while (i < 8)
+        {
+        for (j = 0; j < 8; j++)
+        {
+        putchar(*(*(a + i) + j));
+        }
+        putchar('\n');
+        i++;
+        }
+}
