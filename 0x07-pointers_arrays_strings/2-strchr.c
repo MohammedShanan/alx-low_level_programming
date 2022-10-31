ea2cd409e8e299a0066ef1a9d807a87ecae9326e
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-31 05:15:34 -0700

    solved strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+/**
+ *_strchr - locates a character in a string
+ * @s: string to search
+ * @c: character to be located
+ * Return: pointer to dest
+ */
+char *_strchr(char *s, char c);
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
+
