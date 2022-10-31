b59da11a79ff5a321d4a9c10885287302d798f00
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-31 16:43:18 +0200

    solved chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,15 +7,15 @@
 
 void print_chessboard(char (*a)[8])
 {
-        int i = 0, j = 0;
+    int i = 0, j = 0;
 
-        while (i < 8)
-        {
+    while (i < 8)
+    {
         for (j = 0; j < 8; j++)
         {
-        _putchar(*(*(a + i) + j));
+            _putchar(*(*(a + i) + j));
         }
         _putchar('\n');
         i++;
-        }
+    }
 }
