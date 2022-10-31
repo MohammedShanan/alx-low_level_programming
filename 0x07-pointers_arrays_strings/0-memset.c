06911b172f74a6226488a7e31602e18f73666ead
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-31 03:48:42 -0700

    solved memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,3 @@
-#include<stdio.h>
 /**
  * _memset - fills the first n bytes of the memory area pointed to by
  * s with the constant byte b
@@ -11,10 +10,9 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	 unsigned int i;
 
-	for (i = 0; *(s + i) && i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b;
-		printf("%c\n", *(s + i));
 	}
 	return (s);
 }
