96de5634866e907e7efae53fb785f74d57be4409
mohammedshanan <mohammedshanan12@gmail.com>
2022-10-31 15:13:43 +0200

    solved strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,11 @@
+
 /**
  * _strspn - return length of string that matches values consistently
  * @s: string to search
  * @accept: target matches
  * Return: number of bytes consecutively matched
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int i, j, counter;
