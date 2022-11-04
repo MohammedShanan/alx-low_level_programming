8bc16e49500e570be0f885e107a841636bac9450
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-04 13:20:30 +0200

    solved argc_argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,12 +2,12 @@
 
 /**
  *main - prints the number of arguments passed into it.
- * @argc: argument counter
+  * @argc: argument counter
  * @argv: pointer to array of arguments
  *Return: 0
  */
 
-int main(int argc)
+int main(int argc, char *argv[])
 {
 printf("%i\n", argc);
 return (0);
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,7 +7,7 @@
  *Return: 0
  */
 
-int main(char *argv[])
+int main(int argc, char *argv[])
 {
     int i;
     for (i = 0; argv[i]; i++)
