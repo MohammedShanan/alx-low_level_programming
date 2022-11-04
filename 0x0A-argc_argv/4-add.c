b92ad2e346e3788b846bcd4fd7ed357bf9938cc3
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-04 13:44:21 +0200

    solved argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,40 +7,40 @@
  */
 int is_num(char *argvv)
 {
-    int j = 0;
+int j = 0;
 
-    for (j = 0; argvv[j]; j++)
-    {
-        if (!(argvv[j] >= '0' && argvv[j] <= '9'))
-            return (0);
-    }
-    return (1);
+for (j = 0; argvv[j]; j++)
+{
+if (!(argvv[j] >= '0' && argvv[j] <= '9'))
+return (0);
+}
+return (1);
 }
 /**
  *main - add integers.
- * @argc: argument counter
- * @argv: pointer to array of arguments
- *Return: 0 on success 1 on error
- */
+* @argc: argument counter
+* @argv: pointer to array of arguments
+*Return: 0 on success 1 on error
+*/
 
 int main(int argc, char *argv[])
 {
-    int i, num, sum = 0;
-    if (argc == 1)
-    {
-        printf("0\n");
-        return (0);
-    }
-    for (i = 1; argv[i]; i++)
-    {
-        num = atoi(argv[i]);
-        if (is_num(argv[i]))
-        {
-            printf("Error\n");
-            return (1);
-        }
-        sum += num;
-    }
-    printf("%i\n", sum);
-    return (0);
+int i, num, sum = 0;
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+for (i = 1; argv[i]; i++)
+{
+num = atoi(argv[i]);
+if (!is_num(argv[i]))
+{
+printf("Error\n");
+return (1);
+}
+sum += num;
+}
+printf("%i\n", sum);
+return (0);
 }
