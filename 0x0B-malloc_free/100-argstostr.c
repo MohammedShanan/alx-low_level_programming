d34b788f10a777c4cc745368b820d140b7714765
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-07 14:46:33 +0200

    solved argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,14 +8,14 @@
 
 int _strlen(char *s)
 {
-    int len;
-    len = 0;
-    while (*s)
-    {
-        len++;
-        s++;
-    }
-    return (len);
+int len;
+len = 0;
+while (*s)
+{
+len++;
+s++;
+}
+return (len);
 }
 
 /**
@@ -27,42 +27,46 @@ int _strlen(char *s)
 
 char *str_concat(char *s1, char *s2)
 {
-    char *new_str;
-    int i = 0, j = 0;
-    s1 = s1 == NULL ? "" : s1;
-    s2 = s2 == NULL ? "" : s2;
-    new_str = malloc(_strlen(s1) + _strlen(s2) + 1);
-    if (!new_str)
-    {
-        return (NULL);
-    }
-    while (i < _strlen(s1)) /* concatenate */
-    {
-        *(new_str + i) = *(s1 + i);
-        i++;
-    }
-    while (j < _strlen(s2))
-    {
-        *(new_str + i) = *(s2 + j);
-        i++, j++;
-    }
-    return (new_str);
+char *new_str;
+int i = 0, j = 0;
+s1 = s1 == NULL ? "" : s1;
+s2 = s2 == NULL ? "" : s2;
+new_str = malloc(_strlen(s1) + _strlen(s2) + 1);
+if (!new_str)
+{
+return (NULL);
 }
-
+while (i < _strlen(s1)) /* concatenate */
+{
+*(new_str + i) = *(s1 + i);
+i++;
+}
+while (j < _strlen(s2))
+{
+*(new_str + i) = *(s2 + j);
+i++, j++;
+}
+return (new_str);
+}
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argc
+ * @av: arguments
+ * Return: pointer to array
+ */
 char *argstostr(int ac, char **av)
 {
-    int i;
-    char *new_str = "";
-    char *tmp;
-    if (ac == 0 || av == NULL)
-    {
-        return (NULL);
-    }
-    for (i = 0; i < ac; i++)
-    {
-        tmp = str_concat(av[i], "\n");
-        printf("tmp is %s new is %s\n", tmp, new_str);
-        new_str = str_concat(new_str, tmp);
-    }
-    return (new_str);
+int i;
+char *new_str = "";
+char *tmp;
+if (ac == 0 || av == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < ac; i++)
+{
+tmp = str_concat(av[i], "\n");
+new_str = str_concat(new_str, tmp);
+}
+return (new_str);
 }
