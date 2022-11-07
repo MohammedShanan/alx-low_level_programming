00b24cfcae9419b6e1bc54ed4c45bae8b0cff9eb
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-07 20:12:15 +0200

    solved strtow

--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -2,90 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
- */
-
-int _strlen(char *s)
-{
-    int len;
-    len = 0;
-    while (*s)
-    {
-        len++;
-        s++;
-    }
-    return (len);
-}
-
-int num_of_words(char *text)
-{
-    int i, j = 0;
-    for (i = 0; text[i]; i++)
-    {
-        if (text[i] == ' ')
-        {
-            j++;
-        }
-    }
-    return j + 1;
-}
-char *rm_ext_space(char *s)
-{
-    int i, indx = 0, len = _strlen(s) + 1, len_tmp = 0;
-    char *tmp = malloc(len);
-    char *new_s;
-    for (i = 0; s[i]; i++)
-    {
-        if (s[i] == ' ' && (s[i + 1] == ' ' || s[i + 1] == '\0' || len_tmp == 0))
-        {
-            continue;
-        }
-        tmp[len_tmp++] = s[i];
-    }
-    tmp[len_tmp] = '\0';
-    new_s = malloc(len_tmp);
-    for (indx = 0; indx <= len_tmp; indx++)
-    {
-        new_s[indx] = tmp[indx];
-    }
-    free(tmp);
-    return (new_s);
-}
-
-char **strtow(char *str)
-{
-    char **arr;
-    int i = 0, j = 0, num_words;
-    if (str == NULL || *str == '\0')
-    {
-        return (NULL);
-    }
-    str = rm_ext_space(str);
-    num_words = num_of_words(str);
-    arr = malloc(num_words * sizeof(char *));
-    if (arr == NULL)
-    {
-        free(arr);
-        return (NULL);
-    }
-
-    while (i <= num_words)
-    {
-        if (str[j] == ' ' || str[j] == '\0')
-        {
-            arr[i][j] = '\0';
-            i++, j++;
-            continue;
-        }
-        arr[i][j] = str[j];
-        j++;
-    }
-    return (arr);
-}
-
-/**
  * print_tab - Prints an array of string
  * @tab: The array to print
  *
@@ -100,7 +16,6 @@ void print_tab(char **tab)
         printf("%s\n", tab[i]);
     }
 }
-
 /**
  * main - check the code for ALX School students.
  *
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,4 +1,4 @@
-#include<stdlib.h>
+#include <stdlib.h>
 /**
  * _strlen - returns the length of a string
  * @s: string
@@ -7,78 +7,102 @@
 
 int _strlen(char *s)
 {
-    int len;
-    len = 0;
-    while (*s)
-    {
-        len++;
-        s++;
-    }
-    return (len);
+int len;
+len = 0;
+while (*s)
+{
+len++;
+s++;
 }
-
+return (len);
+}
+/**
+ * num_of_words - count the number of words in a string
+ * @text: string
+ * Return: number of words
+ */
 int num_of_words(char *text)
 {
-    int i, j = 0;
-    for (i = 0; text[i]; i++)
-    {
-        if (text[i] == ' ')
-        {
-            j++;
-        }
-    }
-    return j + 1;
+int i, j = 0;
+for (i = 0; text[i]; i++)
+{
+if (text[i] == ' ')
+{
+j++;
 }
+}
+return j + 1;
+}
+/**
+ * rm_ext_space - remove extra spaces from a string
+ * @s: string
+ * Return: string
+ */
+
 char *rm_ext_space(char *s)
 {
-    int i, indx = 0, len = _strlen(s) + 1, len_tmp = 0;
-    char *tmp = malloc(len);
-    char *new_s;
-    for (i = 0; s[i]; i++)
-    {
-        if (s[i] == ' ' && (s[i + 1] == ' ' || s[i + 1] == '\0' || len_tmp == 0))
-        {
-            continue;
-        }
-        tmp[len_tmp++] = s[i];
-    }
-    tmp[len_tmp] = '\0';
-    new_s = malloc(len_tmp);
-    for (indx = 0; indx <= len_tmp; indx++)
-    {
-        new_s[indx] = tmp[indx];
-    }
-    free(tmp);
-    return (new_s);
+int i, indx = 0, len = _strlen(s) + 1, len_tmp = 0;
+char *tmp = malloc(len);
+char *new_s;
+for (i = 0; s[i]; i++)
+{
+if (s[i] == ' ' && (s[i + 1] == ' ' || s[i + 1] == '\0' || len_tmp == 0))
+{
+continue;
 }
+tmp[len_tmp++] = s[i];
+}
+tmp[len_tmp] = '\0';
+new_s = malloc(len_tmp);
+for (indx = 0; indx <= len_tmp; indx++)
+{
+new_s[indx] = tmp[indx];
+}
+free(tmp);
+return (new_s);
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: string
+ * Return: an array of words
+ */
 
 char **strtow(char *str)
 {
-    char **arr;
-    int i = 0, j = 0, num_words;
-    if (str == NULL || *str == '\0')
-    {
-        return (NULL);
-    }
-    str = rm_ext_space(str);
-    num_words = num_of_words(str);
-    arr = malloc(num_words * sizeof(char *));
-    if (arr == NULL)
-    {
-        free(arr);
-        return (NULL);
-    }
-    
-    while (i <= num_words)
-    {
-        if (str[j] == ' ' || str[j] == '\0')
-        {
-            arr[i][j] = '\0';
-            i++, j++;
-            continue;
-        }
-        arr[i][j] = str[j];
-        j++;
-    }
-    return (arr);
+char **arr;
+char *tmp;
+int i = 0, j = 0, num_words, len_w = 0, indx = 0;
+if (str == NULL || *str == '\0')
+{
+return (NULL);
+}
+tmp = rm_ext_space(str);
+num_words = num_of_words(tmp);
+arr = malloc((num_words + 1) * sizeof(char *));
+if (arr == NULL)
+{
+free(arr);
+return (NULL);
+}
+
+while (i < num_words)
+{
+
+if (tmp[indx] == ' ' || tmp[indx] == '\0')
+{
+arr[i] = malloc(len_w + 1);
+for (j = 0; len_w; j++)
+{
+arr[i][j] = tmp[indx - len_w];
+len_w--;
+}
+arr[i][j] = '\0';
+i++, indx++;
+continue;
+}
+indx++, len_w++;
+}
+arr[i] = NULL;
+return (arr);
 }
