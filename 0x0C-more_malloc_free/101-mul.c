0a6f161fcd1bb1b616ddeb3a43a9f061eea11c5b
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-10 13:56:37 +0200

    initail commit for mul

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,20 @@
+#include<stdio.h>\
+#include<stdint.h>
+#include<stdlib.h>
+/**
+ *main - print the product of two integers
+*Return: 0 on successes , 98 on error;
+*/
+int main(int argc, char const *argv[])
+{
+int n1, n2;
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
+printf("%i\n", n1 * n2);
+return 0;
+}
