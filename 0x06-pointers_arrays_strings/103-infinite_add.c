51e6d3b38d31ac0c0a2b3accea79d4ee436ae379
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-14 15:53:19 +0200

    fixe infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,11 +9,33 @@
  */
 int ctoi(char c)
 {
-    char *num = malloc(2);
-    num[0] = c;
-    num[1] = '\0';
+char *num = malloc(2);
+num[0] = c;
+num[1] = '\0';
 
-    return (atoi(num));
+return (atoi(num));
+}
+/**
+ *  insert_to_buff - insert the result of the addition to the buffer
+ * @result: result of the addition
+ * @r: buffer
+ * @len_r: size of the buffer
+ * Return: buffer
+ */
+char *insert_to_buff(char *result, char *r, int len_r)
+{
+int i = 0, len_result = strlen(result);
+if (len_result + 1 > len_r)
+{
+return (0);
+}
+while (i <= len_result)
+{
+r[i] = result[i];
+i++;
+}
+free(result);
+return (r);
 }
 
 /**
@@ -27,40 +49,39 @@ int ctoi(char c)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len_n1 = strlen(n1);
-    int len_n2 = strlen(n2);
-    int sum, len_buff, carry;
-    sum = carry = 0;
-    len_buff = len_n1 > len_n2 ? len_n1 + 1 : len_n2 + 1;
-    if (size_r < len_buff)
-    {
-        return (0);
-    }
-    r = realloc(r, len_buff + 1);
-    r[len_buff] = '\0';
-    len_n1--, len_n2--, len_buff--;
-    while (len_buff)
-    {
-        if (len_n1 < 0 && len_n2 >= 0)
-        {
-            sum = ctoi(n2[len_n2--]) + carry;
-        }
-        else if (len_n2 < 0 && len_n1 >= 0)
-        {
-            sum = ctoi(n1[len_n1--]) + carry;
-        }
-        else if (len_n1 >= 0 && len_n2 >= 0)
-        {
-            sum = ctoi(n1[len_n1--]) + ctoi(n2[len_n2--]) + carry;
-        }
-        carry = sum / 10;
-        r[len_buff--] = (sum % 10) + '0';
-        sum = 0;
-    }
-    if (carry == 0)
-    {
-        return (++r);
-    }
-    r[0] = carry + '0';
-    return (r);
+int len_n1 = strlen(n1);
+int len_n2 = strlen(n2);
+int sum, len_buff, carry;
+char *buff;
+sum = carry = 0;
+len_buff = len_n1 > len_n2 ? len_n1 + 1 : len_n2 + 1;
+buff = malloc(len_buff + 1);
+buff[len_buff] = '\0';
+len_n1--, len_n2--, len_buff--;
+while (len_buff)
+{
+if (len_n1 < 0 && len_n2 >= 0)
+{
+sum = ctoi(n2[len_n2--]) + carry;
+}
+else if (len_n2 < 0 && len_n1 >= 0)
+{
+sum = ctoi(n1[len_n1--]) + carry;
+}
+else if (len_n1 >= 0 && len_n2 >= 0)
+{
+sum = ctoi(n1[len_n1--]) + ctoi(n2[len_n2--]) + carry;
+}
+carry = sum / 10;
+buff[len_buff--] = (sum % 10) + '0';
+}
+if (carry == 1)
+{
+buff[0] = '1';
+}
+else
+{
+buff++;
+}
+return (insert_to_buff(buff, r, size_r));
 }
