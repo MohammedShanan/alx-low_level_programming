02c515065cfcd00308ab0e8cceb78813887ea16c
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-14 14:10:29 +0200

    fixe infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,155 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/**
- * get_arr_size - create an array to store the number
- * @len: number digits in the number
- * Return: size of the array
- */
-int get_arr_size(int len)
-{
-int array_size;
-
-array_size = len / 9;
-if (array_size * 9 < len)
-{
-array_size += 1;
-}
-return (array_size);
-}
 
 /**
- * create_num_array - create an array to store the number
- * @s: number
- * @new_arr: array to be created
- * @size: size of the array
- * @len: number of digits in the number
- * Return: 1 success, 0 on error
+ *  ctoi - change char to int
+ * @c: charater to be changed
+ * Return: an integere
  */
+int ctoi(char c)
+{
+    char *num = malloc(2);
+    num[0] = c;
+    num[1] = '\0';
 
-int create_num_array(char *s, int new_arr[], int size, int len)
-{
-int start, end, i = 0;
-char *tmp = malloc(10 * sizeof(char));
-if (tmp == NULL)
-{
-return (0);
-}
-
-size -= 1;
-start = len - 9 <= 0 ? 0 : len - 9;
-end = len - 1;
-while (size >= 0)
-{
-while (start + i <= end)
-{
-*(tmp + i) = s[start + i];
-i++;
-}
-tmp[i] = '\0';
-i = 0;
-end = start - 1;
-start = start - 9 <= 0 ? 0 : start - 9;
-new_arr[size] = atoi(tmp);
-size--;
-}
-free(tmp);
-return (1);
-}
-
-/**
- * num_to_buff - write large number from array to buffer
- * @buff: buffer to write to
- * @buff_size: size of the buffer
- * @arr: array containing the number
- * @size_arr: size of the array
- * Return: 1 on success, 0 on error
- */
-
-int num_to_buff(char *buff, int buff_size,
-unsigned int arr[], int size_arr)
-{
-int buff_counter = 0, arr_counter = 0, i = 0;
-unsigned int digit;
-double tens = 1;
-while (tens <= arr[arr_counter])
-{
-tens *= 10;
-i++;
-}
-if (i + 9 * (size_arr - 1) > (buff_size - 1))
-{
-return (0);
-}
-
-while (arr_counter < size_arr && buff_counter < buff_size)
-{
-tens = 1;
-while (tens * 10 <= arr[arr_counter])
-{
-tens *= 10;
-}
-
-while (tens >= 1 && buff_counter < buff_size)
-{
-digit = arr[arr_counter] / tens;
-buff[buff_counter] = digit + '0';
-arr[arr_counter] = arr[arr_counter] - tens *digit;
-tens /= 10;
-buff_counter++;
-}
-arr_counter++;
-}
-buff[buff_counter] = '\0';
-return (1);
-}
-
-/**
- * sum_arrays - sum two arrays
- * @arr1: first number
- * @arr2: second number
- * @size_arr1: size of first array
- * @size_arr2: size of second array
- * @size_sum: a pointer to the size of the first
- * Return: a pointer to the sum array
- */
-
-unsigned int *sum_arrays(int arr1[], int arr2[],
-int size_arr1, int size_arr2, int *size_sum)
-{
-int carry = 0;
-unsigned int max_num = 1000000000, sum = 0;
-int counter = *size_sum - 1;
-unsigned int *sum_array = malloc(*size_sum * sizeof(int));
-
-size_arr1 -= 1, size_arr2 -= 1;
-while (size_arr1 >= 0 || size_arr2 >= 0)
-{
-if (size_arr1 < 0)
-{
-sum = arr2[size_arr2] + carry;
-}
-else if (size_arr2 < 0)
-{
-sum = arr1[size_arr1] + carry;
-}
-else
-{
-sum = arr1[size_arr1] + arr2[size_arr2] + carry;
-}
-carry = sum / max_num;
-sum_array[counter] = sum != max_num ? sum % max_num : max_num;
-size_arr1--, size_arr2--, counter--;
-}
-if (carry == 0 || *(sum_array + 1) == max_num)
-{
-sum_array++, *size_sum -= 1;
-}
-else
-{
-sum_array[0] = 1;
-}
-return (sum_array);
+    return (atoi(num));
 }
 
 /**
@@ -163,26 +27,40 @@ return (sum_array);
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int len_n1 = strlen(n1);
-int len_n2 = strlen(n2);
-int size_arr1 = get_arr_size(len_n1);
-int size_arr2 = get_arr_size(len_n2);
-int size_sum = size_arr1 > size_arr2 ? size_arr1 + 1 : size_arr2 + 1;
-int *arr1 = malloc(size_arr1 *sizeof(int));
-int *arr2 = malloc(size_arr2 * sizeof(int));
-unsigned int *sum_array;
-if (arr1 == NULL || arr2 == NULL)
-{
-free(arr1), free(arr2);
-return (0);
-}
-create_num_array(n1, arr1, size_arr1, len_n1);
-create_num_array(n2, arr2, size_arr2, len_n2);
-
-sum_array = sum_arrays(arr1, arr2, size_arr1, size_arr2, &size_sum);
-if (num_to_buff(r, size_r, sum_array, size_sum) == 0)
-{
-return (0);
-}
-return (r);
+    int len_n1 = strlen(n1);
+    int len_n2 = strlen(n2);
+    int sum, len_buff, carry;
+    sum = carry = 0;
+    len_buff = len_n1 > len_n2 ? len_n1 + 1 : len_n2 + 1;
+    if (size_r < len_buff)
+    {
+        return (0);
+    }
+    r = realloc(r, len_buff + 1);
+    r[len_buff] = '\0';
+    len_n1--, len_n2--, len_buff--;
+    while (len_buff)
+    {
+        if (len_n1 < 0 && len_n2 >= 0)
+        {
+            sum = ctoi(n2[len_n2--]) + carry;
+        }
+        else if (len_n2 < 0 && len_n1 >= 0)
+        {
+            sum = ctoi(n1[len_n1--]) + carry;
+        }
+        else if (len_n1 >= 0 && len_n2 >= 0)
+        {
+            sum = ctoi(n1[len_n1--]) + ctoi(n2[len_n2--]) + carry;
+        }
+        carry = sum / 10;
+        r[len_buff--] = (sum % 10) + '0';
+        sum = 0;
+    }
+    if (carry == 0)
+    {
+        return (++r);
+    }
+    r[0] = carry + '0';
+    return (r);
 }
--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,35 +1,50 @@
 #include <stdio.h>
 #include <stdint.h>
+#include<string.h>
 #include <stdlib.h>
 
 int is_num(char *argvv);
+char *subtract_one(char *n);
+char *infinite_add(char *n1, char *n2);
 
 /**
  *main - print the product of two integers
-* @argc: argument counter
-* @argv: pointer to array of arguments
-*Return: 0 on successes , 98 on error;
-*/
-
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ *Return: 0 on successes , 98 on error;
+ */
 
 int main(int argc, char *argv[])
 {
-unsigned long int n1, n2;
-if (argc != 3)
-{
-printf("Error\n");
-exit(98);
-}
-if (!is_num(argv[1]) || !is_num(argv[2]))
-{
-printf("Error\n");
-exit(98);
-}
+    unsigned long int n1, n2;
+    char *n = "235234693269436436223446526546334576437634765378653875874687649698659586695898579";
+    char *m = "28658034365084365083426083109679137608216408631430814308651084650816406134060831608310853086103769013709675067130586570832760732096730978014607369739567864508634086304807450973045703428580934825098342095832409850394285098342509834209583425345267413639235755891879970464524226159074760914989935413350556875770807019893069201247121855122836389417022552166316010013074258781583143870461182707893577849408672040555089482160343085482612348145322689883025225988799452329290281169927532160590651993511788518550547570284574715925006962738262888617840435389140329668772644708";
+    char *res1 = malloc(1000000000);
+    char *res2 = malloc(1000000000);
+    res1 = n;
+    res2 = "0";
+    while (*res1 != '\0')
+    {
+        res1 = subtract_one(res1);
+        res2 = infinite_add(res2, m);
+    }
+        printf("%s\n", res2);
+        free(res1), free(res2);
+    // if (argc != 3)
+    // {
+    //     printf("Error\n");
+    //     exit(98);
+    // }
+    // if (!is_num(argv[1]) || !is_num(argv[2]))
+    // {
+    //     printf("Error\n");
+    //     exit(98);
+    // }
 
-n1 = atoi(argv[1]);
-n2 = atoi(argv[2]);
-printf("%lu\n", n1 *n2);
-return (0);
+    // n1 = atoi(argv[1]);
+    // n2 = atoi(argv[2]);
+    // printf("%lu\n", n1 * n2);
+    return (0);
 }
 
 /**
@@ -39,12 +54,110 @@ return (0);
  */
 int is_num(char *argvv)
 {
-int j = 0;
+    int j = 0;
 
-for (j = 0; argvv[j]; j++)
-{
-if (!(argvv[j] >= '0' && argvv[j] <= '9'))
-return (0);
+    for (j = 0; argvv[j]; j++)
+    {
+        if (!(argvv[j] >= '0' && argvv[j] <= '9'))
+            return (0);
+    }
+    return (1);
 }
-return (1);
+/**
+ *  ctoi - change char to int
+ * @c: charater to be changed
+ * Return: an integere
+ */
+int ctoi(char c)
+{
+    char *num = malloc(2);
+    num[0] = c;
+    num[1] = '\0';
+
+    return (atoi(num));
+}
+
+    /**
+     * infinite_add - adds two numbers
+     * @n1: first number
+     * @n2: second number
+     * @r: buffer for the result
+     * @size_r: size of the buffer
+     * Return: result buffer on success, 0 on error
+     */
+
+    char *infinite_add(char *n1, char *n2)
+{
+    int len_n1 = strlen(n1);
+    int len_n2 = strlen(n2);
+    int sum, len_buff, carry;
+    char *buff;
+    sum = carry = 0;
+    len_buff = len_n1 > len_n2 ? len_n1 + 1: len_n2 + 1;
+    buff = malloc(len_buff + 1);
+    buff[len_buff] = '\0';
+    len_n1 --, len_n2 --, len_buff--;
+    while (len_buff)
+    {
+        if (len_n1 < 0 && len_n2 >= 0)
+        {
+            sum = ctoi(n2[len_n2--]) + carry;
+        }
+        else if (len_n2 < 0 && len_n1 >= 0)
+        {
+            sum = ctoi(n1[len_n1--]) + carry;
+        }
+        else if (len_n1 >= 0 && len_n2 >= 0)
+        {
+            sum = ctoi(n1[len_n1--]) + ctoi(n2[len_n2--]) + carry;
+        }
+        carry = sum / 10;
+        buff[len_buff--] = (sum % 10) + '0';
+        sum = 0;
+    }
+    if (carry == 0)
+    {
+        return (++buff);
+    }
+    buff[0] = carry + '0';
+    return (buff);
+}
+
+/**
+ *  subtract_one - subtract one from a number
+ * @n: number
+ * Return: number - 1
+ */
+
+char *subtract_one(char *n)
+{
+    int len_n, size_buff;
+    int res;
+    char *buff;
+    len_n = strlen(n);
+    size_buff = strlen(n);
+    buff = malloc(size_buff + 1);
+    buff[size_buff] = '\0';
+    size_buff--, len_n--;
+    if (n[len_n] >= '1')
+    {
+        res = ctoi(n[len_n]) - 1;
+        buff = strdup(n);
+        buff[size_buff] = res + '0';
+    }
+    else
+    {
+        while (n[len_n] == '0')
+        {
+            buff[size_buff--] = '9';
+            len_n--;
+        }
+        res = ctoi(n[len_n]) - 1;
+        buff[size_buff] = res + '0';
+    }
+    while (*buff == '0')
+    {
+        buff++;
+    }
+    return (buff);
 }
