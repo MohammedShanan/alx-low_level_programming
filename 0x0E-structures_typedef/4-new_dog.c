d522b9465954d7fe37306e0f6b0e8bb1b1739129
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-14 13:38:26 +0200

    solved new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,11 +8,11 @@
  */
 int len(char *str)
 {
-int i;
+    int i;
 
-for (i = 0; *(str + i); i++)
-;
-return (i);
+    for (i = 0; *(str + i); i++)
+        ;
+    return (i);
 }
 /**
  * _strcpy - copies the string pointed to by src,
@@ -25,14 +25,14 @@ return (i);
 
 char *_strcpy(char *dest, char *src)
 {
-int i;
+    int i;
 
-for (i = 0; src[i]; i++)
-{
-dest[i] = src[i];
-}
-dest[i] = '\0';
-return (dest);
+    for (i = 0; src[i]; i++)
+    {
+        dest[i] = src[i];
+    }
+    dest[i] = '\0';
+    return (dest);
 }
 /**
  * new_dog - create a new dog
@@ -43,42 +43,42 @@ return (dest);
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *dt;
-char *name_cp, *owner_cp;
-dt = malloc(sizeof(dog_t));
-if (dt == NULL)
-{
-return (NULL);
-}
-dt->age = age;
-if (name != NULL)
-{
-name_cp = malloc(len(name) + 1);
-if (name_cp == NULL)
-{
-free(dt);
-return (NULL);
-}
-dt->name = _strcpy(name_cp, name_cp);
-}
-else
-{
-dt->name = NULL;
-}
-if (owner != NULL)
-{
-owner_cp = malloc(len(owner) + 1);
-if (owner_cp == NULL)
-{
-free(name_cp);
-free(dt);
-return (NULL);
-}
-dt->owner = _strcpy(owner_cp, owner);
-}
-else
-{
-dt->owner = NULL;
-}
-return (dt);
+    dog_t *dt;
+    char *name_cp, *owner_cp;
+    dt = malloc(sizeof(dog_t));
+    if (dt == NULL)
+    {
+        return (NULL);
+    }
+    dt->age = age;
+    if (name != NULL)
+    {
+        name_cp = malloc(len(name) + 1);
+        if (name_cp == NULL)
+        {
+            free(dt);
+            return (NULL);
+        }
+        dt->name = _strcpy(name_cp, name_cp);
+    }
+    else
+    {
+        dt->name = NULL;
+    }
+    if (owner != NULL)
+    {
+        owner_cp = malloc(len(owner) + 1);
+        if (owner_cp == NULL)
+        {
+            free(name_cp);
+            free(dt);
+            return (NULL);
+        }
+        dt->owner = _strcpy(owner_cp, owner);
+    }
+    else
+    {
+        dt->owner = NULL;
+    }
+    return (dt);
 }
