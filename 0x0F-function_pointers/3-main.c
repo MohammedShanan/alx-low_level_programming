bf54e0333bc0d8ffd83e811c7ffe956cb77495c0
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-16 17:29:52 +0200

    solved calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,38 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>  /* printf */
+#include <stdlib.h> /* atoi */
 #include "3-calc.h"
+
 /**
  * main - when user runs main,
  * user should give two integers and an operator and
- * main will calculate the math using a function pointer.
+ * main will calculate the math via a function pointer.
  * prints sum, difference, product, dividend, or remainder
  * @argc: argument counter
  * @argv: arguments
  * Return: 0 on sucess
  */
+
 int main(int argc, char *argv[])
 {
-int a, b, res;
-int (*fun)(int, int);
+int n1, n2;
+int (*f)(int, int);
+
+/* validate input */
 if (argc != 4)
 {
-err;
+printf("Error\n");
 exit(98);
 }
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-fun = get_op_func(argv[2]);
-if (fun == NULL && argv[2][1] != '\0')
+
+/* convert user input to ints and point to correct operator function */
+n1 = atoi(argv[1]);
+n2 = atoi(argv[3]);
+f = get_op_func(argv[2]);
+
+if (f == NULL || (argv[2][1] != '\0'))
 {
-err;
+printf("Error\n");
 exit(99);
 }
-if ((fun == op_mod || fun == op_div) && b == 0)
+if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
 {
-err;
+printf("Error\n");
 exit(100);
 }
-res = fun(a, b);
-printf("%i\n", res);
+
+printf("%d\n", f(n1, n2)); /* calculate via function ptr */
+
 return (0);
 }
