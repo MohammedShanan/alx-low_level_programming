5c2dc4b540106201c54be509ee04d91a155e2ccc
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-16 16:57:00 +0200

    initial commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+/**
+ * array_iterator - execute function on an array via a function ptr
+ * @array: array
+ * @size: size of array
+ * @action: pointer to function that we need to execute
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+if (array == NULL || action == NULL)
+{
+return;
+}
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - when user runs main,
+ * user should give two integers and an operator and
+ * main will calculate the math using a function pointer.
+ * prints sum, difference, product, dividend, or remainder
+ * @argc: argument counter
+ * @argv: arguments
+ * Return: 0 on sucess
+ */
+int main(int argc, char *argv[])
+{
+int a, b, res;
+int (*fun)(int, int);
+if (argc != 4)
+{
+err;
+exit(98);
+}
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+fun = get_op_func(argv[2]);
+if (fun == NULL)
+{
+err;
+exit(99);
+}
+if ((fun == op_div || fun == op_mod) && b == 0)
+{
+err;
+exit(100);
+}
+res = fun(a, b);
+printf("%i\n", res);
+return 0;
+}
