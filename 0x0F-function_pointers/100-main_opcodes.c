ec7b49a7a2e200ab59fc6e166249b92edcd0fbab
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-16 17:47:20 +0200

    test main opcode

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,23 +2,23 @@
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
-    int b, i;
-    int (*f) (int, char **);
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    b = atoi(argv[1]);
-    if (b < 0)
-    {
-        printf("Error\n");
-        return (2);
-    }
-    f = main;
-    for (i = 0; i < b; i++)
-    {
-        printf("%x\n", *(*(f + i)));
-    }
-    return 0;
+int b, i;
+int (*f)(int, char **);
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+b = atoi(argv[1]);
+if (b < 0)
+{
+printf("Error\n");
+return (2);
+}
+f = main;
+for (i = 0; i < b; i++)
+{
+printf("%x\n", *(*(f + i)));
+}
+return 0;
 }
