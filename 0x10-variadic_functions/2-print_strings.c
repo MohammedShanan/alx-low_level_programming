7b59aa143c7e9cdc0a13097a57dc25680bd97469
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-17 11:41:14 +0200

    fixed printf

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -5,7 +5,7 @@
  * @separator: delimiter
  * @n: amount of arguments in list
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 unsigned int i = 0;
 va_list ap;
@@ -20,7 +20,7 @@ while (i < n)
 s = va_arg(ap, char *);
 if (s == NULL)
 {
-printf("(nil)", s);
+printf("(nil)");
 }
 else
 {
