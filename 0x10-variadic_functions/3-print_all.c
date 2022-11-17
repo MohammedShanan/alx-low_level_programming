f32d5d06ced01ff42e34fb5e20270c51c91b5d02
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-17 13:37:11 +0200

    test print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,28 +1,43 @@
 
 #include "variadic_functions.h"
+/**
+ * print_char - prints char
+ * @ap: argument parameter
+ */
+
 void print_char(va_list ap)
 {
-printf("%c", va_arg(ap, int));
+    printf("%c", va_arg(ap, int));
 }
+/**
+ * print_int - prints int
+ * @ap: argument parameter
+ */
 void print_int(va_list ap)
 {
-printf("%i", va_arg(ap, int));
+    printf("%i", va_arg(ap, int));
 }
-
+/**
+ * print_float - prints float
+ * @ap: argument parameter
+ */
 void print_float(va_list ap)
 {
-printf("%f", va_arg(ap, double));
+    printf("%f", va_arg(ap, double));
 }
-
+/**
+ * print_string - prints string
+ * @ap: argument parameter
+ */
 void print_string(va_list ap)
 {
-char *s = va_arg(ap, char *);
-if (s != NULL)
-{
-printf("%s", s);
-return;
-}
-printf("(nil)");
+    char *s = va_arg(ap, char *);
+    if (s != NULL)
+    {
+        printf("%s", s);
+        return;
+    }
+    printf("(nil)");
 }
 /**
  * print_all - print varying input of ints, chars, floats, and strings
@@ -30,33 +45,33 @@ printf("(nil)");
  */
 void print_all(const char *const format, ...)
 {
-va_list ap;
-int i = 0, j = 0;
-char *sep = "";
-void (*func)(va_list);
-pts arr[] = {
-{'c', print_char},
-{'i', print_int},
-{'f', print_float},
-{'s', print_string},
-{'\0', NULL}};
-va_start(ap, format);
-while (format[i] && format != NULL)
-{
-j = 0;
-while (arr[j].id != '\0')
-{
-if (arr[j].id == format[i])
-{
-printf("%s", sep);
-func = arr[j].f;
-func(ap);
-sep = ", ";
-}
-j++;
-}
-i++;
-}
-printf("\n");
-va_end(ap);
+    va_list ap;
+    int i = 0, j = 0;
+    char *sep = "";
+    void (*func)(va_list);
+    pts arr[] = {
+        {'c', print_char},
+        {'i', print_int},
+        {'f', print_float},
+        {'s', print_string},
+        {'\0', NULL}};
+    va_start(ap, format);
+    while (format[i] != '\0' && format != NULL)
+    {
+        j = 0;
+        while (arr[j].id != '\0')
+        {
+            if (arr[j].id == format[i])
+            {
+                printf("%s", sep);
+                func = arr[j].f;
+                func(ap);
+                sep = ", ";
+            }
+            j++;
+        }
+        i++;
+    }
+    printf("\n");
+    va_end(ap);
 }
