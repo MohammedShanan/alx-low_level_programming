f6ed0f80d28934d7048de78ac714736d08b42bb0
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-17 14:25:45 +0200

    test print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -51,8 +51,7 @@ void print_all(const char *const format, ...)
 va_list ap;
 int i = 0, j;
 char *sep = "";
-datatype arr[] = {
-{'c', print_char},
+datatype arr[] = {{'c', print_char},
 {'i', print_int},
 {'f', print_float},
 {'s', print_string},
