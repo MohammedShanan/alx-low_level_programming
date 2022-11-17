c1f61da2863ccd44ecca7e84a0e3a31b849d9b62
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-17 11:56:32 +0200

    fixed print stings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,31 +7,30 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    unsigned int i = 0;
-    va_list ap;
-    char *s;
-    if (n == 0)
-    {
-        return;
-    }
-    va_start(ap, n);
-    while (i < n)
-    {
-        s = va_arg(ap, char *);
-        if (s == NULL)
-        {
-            printf("(nil)");
-        }
-        else
-        {
-            printf("%s", s);
-        }
-        if (separator != NULL && i < n - 1)
-        {
-            printf("%s", separator);
-        }
-        i++;
-    }
-    va_end(ap);
-    printf("\n");
+unsigned int i = 0;
+va_list ap;
+char *s;
+if (n > 0)
+{
+va_start(ap, n);
+while (i < n)
+{
+s = va_arg(ap, char *);
+if (s == NULL)
+{
+printf("(nil)");
+}
+else
+{
+printf("%s", s);
+}
+if (separator != NULL && i < n - 1)
+{
+printf("%s", separator);
+}
+i++;
+}
+va_end(ap);
+}
+printf("\n");
 }
