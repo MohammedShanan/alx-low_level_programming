8973b21ef45206023d75392f7de7970dea1bf015
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-24 16:09:22 +0200

    solved add node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -6,20 +6,20 @@
  * Return: length
  */
 
-int _strlen(char *s)
+int _strlen(const char *s)
 {
-    int len;
-    len = 0;
-    while (*s)
-    {
-        len++;
-        s++;
-    }
-    return (len);
+int len;
+len = 0;
+while (*s)
+{
+len++;
+s++;
+}
+return (len);
 }
 
 /**
- * add_node - add node to beginning of linked list
+ * add_node_end - add node to end of linked list
  * @head: linked list
  * @str: data for new node
  * Return: address of new element, or NULL if failed
@@ -27,29 +27,36 @@ int _strlen(char *s)
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *node = malloc(sizeof(list_t));
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    if (node == NULL)
-    {
-        return (NULL);
-    }
-    if (strdup(str) == NULL)
-    {
-        return (NULL);
-    }
-    node->str = strdup(str);
-    node->len = _strlen(node->str);
-    if (head == NULL)
-    {
-        node->next = NULL;
-    }
-    else
-    {
-        node->next = *head;
-    }
-    *head = node;
-    return (node);
+list_t *node;
+list_t *last_node = NULL;
+last_node = *head;
+if (str == NULL)
+{
+return (NULL);
+}
+if (strdup(str) == NULL)
+{
+return (NULL);
+}
+node = malloc(sizeof(list_t));
+if (node == NULL)
+{
+return (NULL);
+}
+node->str = strdup(str);
+node->len = _strlen(node->str);
+node->next = NULL;
+if (*head == NULL)
+{
+*head = node;
+}
+else
+{
+while (last_node->next)
+{
+last_node = last_node->next;
+}
+last_node->next = node;
+}
+return (node);
 }
