55803bbf7501430dc4de9154c6747aa9a7f73dfd
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-24 16:34:46 +0200

    solved me before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-void before_main() __attribute__((constructor));
-void before_main(void)
+void me_before_main(void) __attribute__((constructor));
+/**
+ * me_before_main - executes this function before main function
+ * Note: To do this task we have to put attribute for these two functions. 
+ * When the attribute is constructor attribute, 
+ * then it will be executed before main(), 
+ * and when the attribute is destructor type, then it will be executed after main().
+ */
+void me_before_main(void)
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
