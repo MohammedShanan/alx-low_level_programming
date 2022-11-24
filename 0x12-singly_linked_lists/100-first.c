b0cce97cac07036da8bb05f347c7082f212c14d0
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-24 16:35:56 +0200

    solved me before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,10 +2,11 @@
 void me_before_main(void) __attribute__((constructor));
 /**
  * me_before_main - executes this function before main function
- * Note: To do this task we have to put attribute for these two functions. 
- * When the attribute is constructor attribute, 
- * then it will be executed before main(), 
- * and when the attribute is destructor type, then it will be executed after main().
+ * Note: To do this task we have to put attribute for these two functions.
+ * When the attribute is constructor attribute,
+ * then it will be executed before main(),
+ * and when the attribute is destructor type,
+ * then it will be executed after main().
  */
 void me_before_main(void)
 {
