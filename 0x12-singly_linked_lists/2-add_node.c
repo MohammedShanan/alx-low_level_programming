17a52e7418811669e96e244face339974cead01a
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-24 14:51:44 +0200

    solved add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -28,11 +28,15 @@ return (len);
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *node = malloc(sizeof(list_t));
+if (node == NULL)
+{
+return (NULL);
+}
 if (str == NULL)
 {
 return (NULL);
 }
-if (node == NULL)
+if (strdup(str) == NULL)
 {
 return (NULL);
 }
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+    int len;
+    len = 0;
+    while (*s)
+    {
+        len++;
+        s++;
+    }
+    return (len);
+}
+
+/**
+ * add_node - add node to beginning of linked list
+ * @head: linked list
+ * @str: data for new node
+ * Return: address of new element, or NULL if failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+    list_t *node = malloc(sizeof(list_t));
+    if (str == NULL)
+    {
+        return (NULL);
+    }
+    if (node == NULL)
+    {
+        return (NULL);
+    }
+    node->str = strdup(str);
+    if (node->str == NULL)
+    {
+        return (NULL);
+    }
+    node->len = _strlen(node->str);
+    if (head == NULL)
+    {
+        node->next = NULL;
+    }
+    else
+    {
+        node->next = *head;
+    }
+    *head = node;
+    return (node);
+}
