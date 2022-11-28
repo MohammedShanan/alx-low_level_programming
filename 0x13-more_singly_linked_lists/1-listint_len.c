3facdc0f6dcbe87c9059faca9bf9e92af4876e94
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-28 11:32:35 +0200

    solved add nodeint

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,7 +3,7 @@
  * listint_len - returns the number of elements in a linked listint_t list
  * @h: list
  * Return: number of nodes in list
- */1
+ */
 size_t listint_len(const listint_t *h)
 {
 int counter = 0;
