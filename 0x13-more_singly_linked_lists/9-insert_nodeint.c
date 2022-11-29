bf572f0813bb909a58def94f8f83bd91a607d9fa
mohammedshanan <mohammedshanan12@gmail.com>
2022-11-29 11:39:57 +0200

    fixed insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 listint_t *new_node, *tmp;
 unsigned int i = 0;
 tmp = *head;
-while (tmp && idx)
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->n = n;
+if (idx == 0)
+{
+new_node->next = *head;
+*head = new_node;
+}
+else
+{
+while (tmp)
 {
 if (i == idx - 1)
 {
@@ -22,20 +35,9 @@ i++;
 }
 if (tmp == NULL)
 {
+free(new_node);
 return (NULL);
 }
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
-{
-return (NULL);
-}
-new_node->n = n;
-if (idx == 0)
-{
-new_node->next = *head;
-}
-else
-{
 new_node->next = tmp->next;
 tmp->next = new_node;
 }
