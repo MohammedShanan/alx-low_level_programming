c9ecffa641f3cdc369993316ca27f87539c46fdb
mohammedshanan <mohammedshanan12@gmail.com>
2022-12-01 19:23:00 +0200

    solved flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * flip_bits - determine how many bits to flip to get from one num to another
+ * @n: number
+ * @m: number2
+ * Return: how many bits differ
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int diff;
+unsigned int bits = 0;
+diff = n ^ m;
+do
+{
+bits += diff & 1;
+diff >>= 1;
+} while (diff > 0);
+return (bits);
+}
