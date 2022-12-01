dc7ab88130a62bf5ebef520d01644bd56760a7f9
mohammedshanan <mohammedshanan12@gmail.com>
2022-12-01 17:27:03 +0200

    solved print bin

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,19 +6,19 @@
  */
 void print_binary(unsigned long int n)
 {
-    unsigned long int cp_n = n, mask = 1;
-    int len_n = 0;
-    while (cp_n > 0)
-    {
-        len_n++;
-        cp_n >>= 1;
-    }
-    len_n--;
-    if (len_n > 0)
-        mask = mask << len_n;
-    while (mask > 0)
-    {
-        _putchar(n & mask ? '1' : '0');
-        mask >>= 1;
-    }
+unsigned long int cp_n = n, mask = 1;
+int len_n = 0;
+while (cp_n > 0)
+{
+len_n++;
+cp_n >>= 1;
+}
+len_n--;
+if (len_n > 0)
+mask = mask << len_n;
+while (mask > 0)
+{
+_putchar(n & mask ? '1' : '0');
+mask >>= 1;
+}
 }
