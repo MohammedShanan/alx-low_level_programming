d6fc400aa2326005e89eee150983ec19dec7b5bb
mohammedshanan <mohammedshanan12@gmail.com>
2022-12-01 17:36:39 +0200

    solved get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,5 +1,17 @@
 #include "main.h"
+/**
+ * get_bit - get bit at index
+ * @n: number
+ * @index: index within binary number
+ * Return: bit 0 or 1, or -1 if error
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    
+unsigned int max_bits;
+max_bits = malloc(sizeof(unsigned long int) * 8);
+if (index > max_bits)
+{
+return (-1);
+}
+return ((n >> index) & 1);
 }
