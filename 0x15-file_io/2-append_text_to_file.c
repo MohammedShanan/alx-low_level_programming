e1e5c6bfffc8c0d2b230210c4cd9e98a28228636
mohammedshanan <mohammedshanan12@gmail.com>
2022-12-05 13:10:51 +0200

    solved append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,14 +8,14 @@
 
 int _strlen(char *s)
 {
-    int len;
-    len = 0;
-    while (*s)
-    {
-        len++;
-        s++;
-    }
-    return (len);
+int len;
+len = 0;
+while (*s)
+{
+len++;
+s++;
+}
+return (len);
 }
 
 /**
@@ -27,27 +27,27 @@ int _strlen(char *s)
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, chars_wrote;
-    if (!filename)
-    {
-        return (-1);
-    }
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-    {
-        return (-1);
-    }
-    if (!text_content)
-    {
-        close(fd);
-        return (1);
-    }
-    chars_wrote = write(fd, text_content, _strlen(text_content));
-    if (chars_wrote == -1 || chars_wrote != _strlen(text_content))
-    {
-        close(fd);
-        return (-1);
-    }
-    close(fd);
-    return (1);
+int fd, chars_wrote;
+if (!filename)
+{
+return (-1);
+}
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+{
+return (-1);
+}
+if (!text_content)
+{
+close(fd);
+return (1);
+}
+chars_wrote = write(fd, text_content, _strlen(text_content));
+if (chars_wrote == -1 || chars_wrote != _strlen(text_content))
+{
+close(fd);
+return (-1);
+}
+close(fd);
+return (1);
 }
