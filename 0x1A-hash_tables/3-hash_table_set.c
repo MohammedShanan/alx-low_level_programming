c8cd28b69f604f85c3a7224d714d1d63df958316
mohammedshanan <mohammedshanan12@gmail.com>
2023-02-02 10:30:45 +0200

    solved # table get

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -58,7 +58,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     node = (ht->array)[indx];
     while (node)
     {
-        if (strcmp(key, node->key))
+        if (strcmp(key, node->key) == 0)
         {
             v = strdup(value);
             if (v == NULL)
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - given key, get value
+ * @ht: hash table
+ * @key: key
+ * Return: value; or NULL if not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    hash_node_t *node;
+    unsigned long int indx;
+    indx = key_index((const unsigned char *)key, ht->size);
+    if (ht == NULL || !key)
+    {
+        return (NULL);
+    }
+    node = (ht->array)[indx];
+    while (node)
+    {
+        if (strcmp(key, node->key) == 0)
+        {
+            return (node->value);
+        }
+        node = node->next;
+    }
+    return (NULL);
+}
