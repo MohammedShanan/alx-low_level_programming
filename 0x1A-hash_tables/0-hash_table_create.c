3ccfa8afffe7b72dbf4c28f85673467dd5e0dda5
mohammedshanan <mohammedshanan12@gmail.com>
2023-02-02 23:25:26 +0200

    fixed style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_create - create hash table given size
  * @size: size
@@ -10,15 +9,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 hash_table_t *ht;
 hash_node_t **array;
 unsigned long int i = 0;
-
 if (size == 0)
+{
 return (NULL);
+}
 
-ht = malloc(sizeof(hash_table_t));
-if (!ht)
+ht = malloc(sizeof(hash_node_t));
+if (ht == NULL)
+{
 return (NULL);
+}
+
 array = malloc(sizeof(*array) * size);
-if (!array)
+if (array == NULL)
 {
 free(ht);
 return (NULL);
@@ -28,9 +31,7 @@ while (i < size)
 array[i] = NULL;
 i++;
 }
-
 ht->size = size;
 ht->array = array;
-
 return (ht);
 }
